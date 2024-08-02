eb0c8a282fea72ff28df64fe1b7f30488933fb73
vicky <a>
2024-08-03 01:30:44 +0200

    add brake light

--- Src/main.c
+++ Src/main.c
@@ -795,6 +795,7 @@ int main(void)
 				else int32_temp_current_target=0;
 
 #endif
+				HAL_GPIO_WritePin(BRAKE_LIGHT_GPIO_Port, BRAKE_LIGHT_Pin, GPIO_PIN_SET);
 				int32_temp_current_target= -map(MS.Voltage*CAL_V,BATTERYVOLTAGE_MAX-1000,BATTERYVOLTAGE_MAX,int32_temp_current_target,0);
 			}
 
@@ -804,6 +805,7 @@ int main(void)
 			else if(ui8_Push_Assist_flag)int32_temp_current_target=(MS.assist_level*PUSHASSIST_CURRENT)>>8; //does not work for BAFANG and Kunteng protocol actually
 			// last priority normal ride conditiones
 			else {
+				HAL_GPIO_WritePin(BRAKE_LIGHT_GPIO_Port, BRAKE_LIGHT_Pin, GPIO_PIN_RESET);
 
 #ifdef TS_MODE //torque-sensor mode
 				//calculate current target form torque, cadence and assist level
